3066f59a7b573e007c9c4a5e6df803ccd1adcca6
ARMINA <armindavoudi1997@gmail.com>
2020-07-07 10:03:12 +0430

    Add project files.

---
+++ WorkWithFiles/WorkWithFiles.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    string line;
+    fstream myfile("example.bin", ios::binary | ios::out | ios::in);
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+
+    else cout << "unable to open file";
+
+    return 0;
+}
